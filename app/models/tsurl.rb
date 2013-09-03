33d1001618c4db933def704b4338bf3dc9406186
vld <vld@passenger.com>
2013-09-03 14:46:52 +0300

    Fix logs

--- app/models/tsurl.rb
+++ app/models/tsurl.rb
@@ -3,13 +3,13 @@ class Tsurl < ActiveRecord::Base
   after_create :create_short
 
   def add_to_log(request)
-    log << "Access from #{request.remote_ip} at #{Time.now}\n"
-    save
+    self.log += "Access from #{request.remote_ip} at #{Time.now}\n"
+    self.save
   end
   private
 
   def create_short
-    self.log = "Created at #{Time.now}"
+    self.log = "Created at #{Time.now}\n"
     self.short = self.id.to_s(27).tr("0-9a-q", "a-z")
     self.save
   end
